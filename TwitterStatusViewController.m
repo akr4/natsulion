54d5a5dcd82d406935022811b3f7dc7c46796db0
akira <akira@5e439dd1-9381-4ddc-8459-9b31d6a87cac>
2007-12-29 20:35:07 +0000

    layout change
    git-svn-id: http://www.physalis.net/repos/natsulion/trunk@53 5e439dd1-9381-4ddc-8459-9b31d6a87cac

---
+++ TwitterStatusViewController.m
@@ -0,0 +1,73 @@
+#import "TwitterStatusViewController.h"
+
+
+@implementation TwitterStatusViewController
+
+- (id) initWithTwitterStatus:(TwitterStatus*)status {
+    [super init];
+
+    _status = status;
+    [_status retain];
+    
+    if (![NSBundle loadNibNamed: @"TwitterStatusView" owner: self]) {
+        NSLog(@"unable to load Nib TwitterStatusView.nib");
+    }
+    [textField setStringValue:[status text]];
+    [nameField setStringValue:[[[status name] stringByAppendingString:@"/"] stringByAppendingString:[status screenName]]];
+    [iconView setImage:[status icon]];
+    [timestampField setStringValue:[[status timestamp]      
+                                    descriptionWithCalendarFormat:@"%02H:%02M:%02S"
+                                    timeZone:[NSTimeZone localTimeZone]
+                                    locale:[[NSUserDefaults standardUserDefaults]
+                                            dictionaryRepresentation]]];
+    
+    return self;
+}
+
+- (TwitterStatus*) status {
+    return _status;
+}
+
+- (NSView*) view {
+    return view;
+}
+
+- (void) dealloc {
+    [_status release];
+    [super dealloc];
+}
+
+- (NSTextField*) nameField {
+    return nameField;
+}
+
+- (BOOL) isEqual:(id)anObject {
+//    NSLog(@"%s", __PRETTY_FUNCTION__);
+	if (anObject == self)
+        return TRUE;
+	if (!anObject || ![anObject isKindOfClass:[self class]])
+        return FALSE;
+    return [[self status] isEqual:[(TwitterStatusViewController*)anObject status]];
+}
+     
+- (void) highlight {
+    [view highlight];
+    [textField highlight];
+    [nameField highlight];
+    [timestampField highlight];
+    [iconView highlight];
+}
+
+- (void) lowlight {
+    [view lowlight];
+    [textField lowlight];
+    [nameField lowlight];
+    [timestampField lowlight];
+    [iconView lowlight];
+}
+
+- (replyType_t) replyType {
+    return NORMAL;
+}
+
+@end
