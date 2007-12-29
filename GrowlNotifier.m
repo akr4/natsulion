03d62af27ddf10ea2ec2cb76aa49b7ee4c5ade02
akira <akira@5e439dd1-9381-4ddc-8459-9b31d6a87cac>
2007-12-29 20:34:58 +0000

    layout change
    git-svn-id: http://www.physalis.net/repos/natsulion/trunk@24 5e439dd1-9381-4ddc-8459-9b31d6a87cac

---
+++ GrowlNotifier.m
@@ -0,0 +1,32 @@
+#import <Growl/Growl.h>
+#import "GrowlNotifier.h"
+
+#define NOTIFICATION_MESSAGE_RECEIVED @"Message Received"
+
+@implementation GrowlNotifier
+
+- (id) init {
+    [GrowlApplicationBridge setGrowlDelegate:self];
+    return self;
+}
+
+- (NSDictionary *) registrationDictionaryForGrowl {
+    NSMutableDictionary *d = [[[NSMutableDictionary alloc] initWithCapacity:10] autorelease];
+    NSArray *notifications = [[[NSArray alloc] initWithObjects:NOTIFICATION_MESSAGE_RECEIVED, nil] autorelease];
+    [d setObject:notifications forKey:GROWL_NOTIFICATIONS_ALL];
+    [d setObject:notifications forKey:GROWL_NOTIFICATIONS_DEFAULT];
+    return d;
+}
+
+- (void) sendToGrowlTitle:(NSString*)title andDescription:(NSString*)description andIcon:(NSData*)iconData andPriority:(int)priority andSticky:(BOOL)sticky {
+    [GrowlApplicationBridge
+     notifyWithTitle:title
+     description:description
+     notificationName:NOTIFICATION_MESSAGE_RECEIVED
+     iconData:iconData
+     priority:priority
+     isSticky:sticky
+     clickContext:nil];
+}
+
+@end
