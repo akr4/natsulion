4f39ff4705d65b8c9d74e1d3a78b7153ef2b000c
akira <akira@5e439dd1-9381-4ddc-8459-9b31d6a87cac>
2007-12-29 20:35:09 +0000

    layout change
    git-svn-id: http://www.physalis.net/repos/natsulion/trunk@60 5e439dd1-9381-4ddc-8459-9b31d6a87cac

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NatsuLion
+//
+//  Created by ?? ?? on 07/12/14.
+//  Copyright __MyCompanyName__ 2007. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
