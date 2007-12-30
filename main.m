6851cd6f1ac8b47a0090674d81485182dc58beb6
akira <akira@5e439dd1-9381-4ddc-8459-9b31d6a87cac>
2007-12-30 16:59:22 +0000

    move from APL to modified BSD license
    git-svn-id: http://www.physalis.net/repos/natsulion/trunk@72 5e439dd1-9381-4ddc-8459-9b31d6a87cac

--- main.m
+++ main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  NatsuLion
-//
-//  Created by ?? ?? on 07/12/14.
-//  Copyright __MyCompanyName__ 2007. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, char *argv[])
