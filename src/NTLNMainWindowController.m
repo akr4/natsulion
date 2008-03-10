203177102156ede4e8b7ab641a30d5f085fd0fcb
akira <akira@5e439dd1-9381-4ddc-8459-9b31d6a87cac>
2008-03-10 23:59:18 +0000

    NTLN-141 resolved. fix a bug which level indicator is shown in status bar by mistake.
    git-svn-id: http://www.physalis.net/repos/natsulion/trunk@259 5e439dd1-9381-4ddc-8459-9b31d6a87cac

--- src/NTLNMainWindowController.m
+++ src/NTLNMainWindowController.m
@@ -185,6 +185,7 @@
     
     [statisticsTextField setToolTip:@"number of posted messages"];
     [messagePostLevelIndicator setToolTip:@"timeline speed"];
+    [messagePostLevelIndicator setHidden:![[NTLNConfiguration instance] showMessageStatisticsOnStatusBar]];
     
     //    NSColor *semiTransparentBlue =
 //    [NSColor colorWithDeviceRed:0.0 green:0.0 blue:1.0 alpha:0.5];
